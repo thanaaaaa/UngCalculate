9a19f319dac796409ebd087da8059b4808c4ab6e
MasterUNG <phrombutr@gmail.com>
2018-01-09 15:18:26 +0700

    Replace Fragment

--- app/src/main/java/masterung/androidthai/in/th/ungcalculate/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/ungcalculate/fragment/MainFragment.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import masterung.androidthai.in.th.ungcalculate.MainActivity;
 import masterung.androidthai.in.th.ungcalculate.R;
@@ -25,8 +27,46 @@ public class MainFragment extends Fragment{
 //        Create Toolbar
         createToolbar();
 
+//        ShowName Controller
+        showNameController();
+
+//        Calculate Controller
+        calculateController();
 
     }   // Main Method
+
+    private void calculateController() {
+
+        Button button = getView().findViewById(R.id.btnCalculate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new CalculateFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+
+    }
+
+    private void showNameController() {
+
+        Button button = getView().findViewById(R.id.btnShowName);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView textView = getView().findViewById(R.id.txtShowName);
+                textView.setText("Master UNG");
+
+            }
+        });
+
+    }
 
     private void createToolbar() {
 
