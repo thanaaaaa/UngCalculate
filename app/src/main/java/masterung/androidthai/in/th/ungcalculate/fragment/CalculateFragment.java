830465972210cdc3e40f9bda097479d8da231e8c
MasterUNG <phrombutr@gmail.com>
2018-01-09 15:43:57 +0700

    Back Controller

--- app/src/main/java/masterung/androidthai/in/th/ungcalculate/fragment/CalculateFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/ungcalculate/fragment/CalculateFragment.java
@@ -3,10 +3,12 @@ package masterung.androidthai.in.th.ungcalculate.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import masterung.androidthai.in.th.ungcalculate.MainActivity;
 import masterung.androidthai.in.th.ungcalculate.R;
 
 /**
@@ -14,6 +16,38 @@ import masterung.androidthai.in.th.ungcalculate.R;
  */
 
 public class CalculateFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+
+    }   // Main Method
+
+    private void createToolbar() {
+
+        Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setTitle("Calculate");
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }
 
     @Nullable
     @Override
